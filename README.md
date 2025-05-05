3443a07ecc0efc00a26a00b073186ba4bf553829
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2025-05-06 01:01:54 +0200

    major update

--- README.md
+++ README.md
@@ -1,56 +1,53 @@
-# Luke's Auto-Rice Bootstrapping Scripts (LARBS)
+# Gerhard's Auto-Rice Bootstrapping Script (GARBS)
 
 ## Installation:
 
 On an Arch-based distribution as root, run the following:
 
 ```
-curl -LO larbs.xyz/larbs.sh
-sh larbs.sh
+curl -LO https://raw.githubusercontent.com/GerhardGustavsen/GARBS/master/garbs.sh
+sh garbs.sh
 ```
 
 That's it.
 
-## What is LARBS?
+## What is GARBS?
 
-LARBS is a script that autoinstalls and autoconfigures a fully-functioning
-and minimal terminal-and-vim-based Arch Linux environment.
+GARBS is a fork of LARBS that autoinstalls and configures a minimal,
+efficient Arch Linux desktop using XFCE as a base environment,
+with optional tiling WM enhancements and privacy-conscious settings.
 
-LARBS can be run on a fresh install of Arch or Artix Linux, and provides you
-with a fully configured diving-board for work or more customization.
+GARBS is designed for users who want a practical, hardware-compatible system
+with minimal bloat and maximum control, built from curated packages and
+tested configurations.
+
+It supports both fresh Arch and Artix installs.
 
 ## Customization
 
-By default, LARBS uses the programs [here in progs.csv](rice/progs.csv) and installs
-[my dotfiles repo (voidrice) here](https://github.com/lukesmithxyz/voidrice),
-but you can easily change this by either modifying the default variables at the
-beginning of the script or giving the script one of these options:
+By default, GARBS uses the programs listed in [progs.csv](progs/progs.csv) and installs
+[Gerhard's dotfiles](https://github.com/GerhardGustavsen/dotfiles),
+but you can customize it by editing the variables at the top of the script or by using these options:
 
 - `-r`: custom dotfiles repository (URL)
 - `-p`: custom programs list/dependencies (local file or URL)
 - `-a`: a custom AUR helper (must be able to install with `-S` unless you
-  change the relevant line in the script
+  change the relevant line in the script)
 
 ### The `progs.csv` list
 
-LARBS will parse the given programs list and install all given programs. Note
-that the programs file must be a three column `.csv`.
+GARBS reads this CSV to determine what to install. It must have three columns:
 
-The first column is a "tag" that determines how the program is installed, ""
-(blank) for the main repository, `A` for via the AUR or `G` if the program is a
-git repository that is meant to be `make && sudo make install`ed.
-
-The second column is the name of the program in the repository, or the link to
-the git repository, and the third column is a description (should be a verb
-phrase) that describes the program. During installation, LARBS will print out
-this information in a grammatical sentence. It also doubles as documentation
-for people who read the CSV and want to install my dotfiles manually.
+1. **Tag**: install method
+   - `""` (blank) for `pacman`
+   - `A` for `yay`/AUR packages
+   - `G` for `git` repositories (compiled with `make`)
+   - `P` for `pip` packages (Python)
+2. **Program**: name in repo, AUR name, or git URL
+Descriptions must be quoted if they contain commas.
 
 Depending on your own build, you may want to tactically order the programs in
-your programs file. LARBS will install from the top to the bottom.
-
-If you include commas in your program descriptions, be sure to include double
-quotes around the whole description to ensure correct parsing.
+your programs file. GARBS will install from the top to the bottom.
 
 ### The script itself
 
@@ -63,7 +60,7 @@ which commands to run to install it. You can easily add new methods of
 installations and tags as well.
 
 Note that programs from the AUR can only be built by a non-root user. What
-LARBS does to bypass this by default is to temporarily allow the newly created
+GARBS does to bypass this by default is to temporarily allow the newly created
 user to use `sudo` without a password (so the user won't be prompted for a
 password multiple times in installation). This is done ad-hocly, but
 effectively with the `newperms` function. At the end of installation,
--- garbs.sh
+++ garbs.sh
@@ -7,7 +7,7 @@
 ### OPTIONS AND VARIABLES ###
 
 dotfilesrepo="https://github.com/GerhardGustavsen/dotfiles.git"
-progsfile="https://raw.githubusercontent.com/GerhardGustavsen/GARBS/blob/master/rice/progs.csv"
+progsfile="https://raw.githubusercontent.com/GerhardGustavsen/GARBS/blob/master/progs/progs.csv"
 aurhelper="yay"
 repobranch="master"
 export TERM=ansi
@@ -52,11 +52,11 @@ usercheck() {
 	! { id -u "$name" >/dev/null 2>&1; } ||
 		whiptail --title "WARNING" --yes-button "CONTINUE" \
 			--no-button "No wait..." \
-			--yesno "The user \`$name\` already exists on this system. LARBS can install for a user already existing, but it will OVERWRITE any conflicting settings/dotfiles on the user account.\\n\\nLARBS will NOT overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that LARBS will change $name's password to the one you just gave." 14 70
+			--yesno "The user \`$name\` already exists on this system. GARBS can install for a user already existing, but it will OVERWRITE any conflicting settings/dotfiles on the user account.\\n\\nGARBS will NOT overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that GARBS will change $name's password to the one you just gave." 14 70
 }
 
 preinstallmsg() {
-	whiptail --title "Let's get this party started!" --yes-button "Let's go!" \
+	whiptail --title "Start instalations?" --yes-button "Let's go!" \
 		--no-button "No, nevermind!" \
 		--yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || {
 		clear
@@ -114,7 +114,7 @@ manualinstall() {
 
 maininstall() {
 	# Installs all needed programs from main repo.
-	whiptail --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 9 70
+	whiptail --title "GARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 9 70
 	installpkg "$1"
 }
 
@@ -122,7 +122,7 @@ gitmakeinstall() {
 	progname="${1##*/}"
 	progname="${progname%.git}"
 	dir="$repodir/$progname"
-	whiptail --title "LARBS Installation" \
+	whiptail --title "GARBS Installation" \
 		--infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 8 70
 	sudo -u "$name" git -C "$repodir" clone --depth 1 --single-branch \
 		--no-tags -q "$1" "$dir" ||
@@ -137,14 +137,14 @@ gitmakeinstall() {
 }
 
 aurinstall() {
-	whiptail --title "LARBS Installation" \
+	whiptail --title "GARBS Installation" \
 		--infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 9 70
 	echo "$aurinstalled" | grep -q "^$1$" && return 1
 	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
 }
 
 pipinstall() {
-	whiptail --title "LARBS Installation" \
+	whiptail --title "GARBS Installation" \
 		--infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 9 70
 	[ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
 	yes | pip install "$1"
@@ -195,7 +195,7 @@ makeuserjs(){
 	arkenfox="$pdir/arkenfox.js"
 	overrides="$pdir/user-overrides.js"
 	userjs="$pdir/user.js"
-	ln -fs "/home/$name/.config/firefox/larbs.js" "$overrides"
+	ln -fs "/home/$name/.config/firefox/garbs.js" "$overrides"
 	[ ! -f "$arkenfox" ] && curl -sL "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js" > "$arkenfox"
 	cat "$arkenfox" "$overrides" > "$userjs"
 	chown "$name:wheel" "$arkenfox" "$userjs"
@@ -209,8 +209,8 @@ makeuserjs(){
 Operation = Upgrade
 Type = Package
 Target = firefox
-Target = librewolf
-Target = librewolf-bin
+Target = firefox
+Target = firefox-bin
 [Action]
 Description=Update Arkenfox user.js
 When=PostTransaction
@@ -271,12 +271,12 @@ refreshkeys ||
 	error "Error automatically refreshing Arch keyring. Consider doing so manually."
 
 for x in curl ca-certificates base-devel git ntp zsh; do
-	whiptail --title "LARBS Installation" \
+	whiptail --title "GARBS Installation" \
 		--infobox "Installing \`$x\` which is required to install and configure other programs." 8 70
 	installpkg "$x"
 done
 
-whiptail --title "LARBS Installation" \
+whiptail --title "GARBS Installation" \
 	--infobox "Synchronizing system time to ensure successful and secure installation of software..." 8 70
 ntpd -q -g >/dev/null 2>&1
 
@@ -286,8 +286,8 @@ adduserandpass || error "Error adding username and/or password."
 
 # Allow user to run sudo without password. Since AUR programs must be installed
 # in a fakeroot environment, this is required for all builds with AUR.
-trap 'rm -f /etc/sudoers.d/larbs-temp' HUP INT QUIT TERM PWR EXIT
-echo "%wheel ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/larbs-temp
+trap 'rm -f /etc/sudoers.d/garbs-temp' HUP INT QUIT TERM PWR EXIT
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/garbs-temp
 
 # Make pacman colorful, concurrent downloads and Pacman eye-candy.
 grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
@@ -313,7 +313,7 @@ putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
 rm -rf "/home/$name/.git/" "/home/$name/README.md" "/home/$name/LICENSE" "/home/$name/FUNDING.yml"
 
 # Install vim plugins if not alread present.
-[ ! -f "/home/$name/.config/nvim/autoload/plug.vim" ] && vimplugininstall
+## [ ! -f "/home/$name/.config/nvim/autoload/plug.vim" ] && vimplugininstall
 
 # Most important command! Get rid of the beep!
 rmmod pcspkr
@@ -345,11 +345,12 @@ EndSection' >/etc/X11/xorg.conf.d/40-libinput.conf
 
 whiptail --infobox "Setting browser privacy settings and add-ons..." 7 60
 
-browserdir="/home/$name/.librewolf"
+browserdir="/home/$name/.firefox"
 profilesini="$browserdir/profiles.ini"
 
-# Start librewolf headless so it generates a profile. Then get that profile in a variable.
-sudo -u "$name" librewolf --headless >/dev/null 2>&1 &
+
+# Start firefox headless so it generates a profile. Then get that profile in a variable.
+sudo -u "$name" firefox --headless >/dev/null 2>&1 &
 sleep 1
 profile="$(sed -n "/Default=.*.default-release/ s/.*=//p" "$profilesini")"
 pdir="$browserdir/$profile"
@@ -358,14 +359,14 @@ pdir="$browserdir/$profile"
 
 [ -d "$pdir" ] && installffaddons
 
-# Kill the now unnecessary librewolf instance.
-pkill -u "$name" librewolf
+# Kill the now unnecessary firefox instance.
+pkill -u "$name" firefox
 
 # Allow wheel users to sudo with password and allow several system commands
 # (like `shutdown` to run without password).
-echo "%wheel ALL=(ALL:ALL) ALL" >/etc/sudoers.d/00-larbs-wheel-can-sudo
-echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/pacman -Syyuw --noconfirm,/usr/bin/pacman -S -u -y --config /etc/pacman.conf --,/usr/bin/pacman -S -y -u --config /etc/pacman.conf --" >/etc/sudoers.d/01-larbs-cmds-without-password
-echo "Defaults editor=/usr/bin/nvim" >/etc/sudoers.d/02-larbs-visudo-editor
+echo "%wheel ALL=(ALL:ALL) ALL" >/etc/sudoers.d/00-garbs-wheel-can-sudo
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/pacman -Syyuw --noconfirm,/usr/bin/pacman -S -u -y --config /etc/pacman.conf --,/usr/bin/pacman -S -y -u --config /etc/pacman.conf --" >/etc/sudoers.d/01-garbs-cmds-without-password
+echo "Defaults editor=/usr/bin/nvim" >/etc/sudoers.d/02-garbs-visudo-editor
 mkdir -p /etc/sysctl.d
 echo "kernel.dmesg_restrict = 0" > /etc/sysctl.d/dmesg.conf
 
